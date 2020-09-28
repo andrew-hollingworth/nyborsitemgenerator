c5d25dc73b4191c69ff5a2ee335d80f600628e6c
Andrew Hollingworth <andrew.hollingworth@gmail.com>
2020-09-28 17:38:07 -0400

    Naming logic v2. dynamic prefix/suffix logic

--- enchantmenttest.js
+++ enchantmenttest.js
@@ -1,14 +1,32 @@
-event.value = this.getField("Enchantment 1");
+var enchant1 = this.getField("Enchantment 1").value.split('-')
+var enchant2 = this.getField("Enchantment 2").value.split('-')
+var enchant3 = this.getField("Enchantment 3").value.split('-')
 
-
-switch (this.getField("Enchantment 1").value.split('-')[0]) {
-  case "0":
-    event.value = this.getField("ItemSubType").value + " of " + this.getField("Enchantment 1").value.split('-')[1];
-    break;
-  case "1":
-    event.value = this.getField("Enchantment 1").value.split('-')[1] + " " + this.getField("ItemSubType").value;
-    break;
-  default:
-    event.value = "";
-    break;
-};
+if ((enchant1[0] + enchant2[0] >= 1) && enchant1[0] != enchant2[0]) {
+  switch (enchant1[0]) {
+    case "0":
+      event.value = enchant2[1] + " " + this.getField("ItemSubType").value + " of " + enchant1[1];
+      break;
+    case "1":
+      event.value = enchant1[1] + " " + this.getField(" ItemSubType").value + " of " + enchant2[1];
+      break;
+  };
+} else if (enchant3[0] >= 0 && (enchant1[0] === enchant2[0] && enchant1[0] != enchant3[0])) {
+  switch (enchant1[0]) {
+    case "0":
+      event.value = enchant3[1] + " " + this.getField("ItemSubType").value + " of " + enchant1[1];
+      break;
+    case "1":
+      event.value = enchant1[1] + " " + this.getField("ItemSubType").value + " of " + enchant3[1];
+      break;
+  };
+} else if (0 <= enchant1[0] <= 1) {
+  switch (this.getField("Enchantment 1").value.split('-')[0]) {
+    case "0":
+      event.value = this.getField("ItemSubType").value + " of " + this.getField("Enchantment 1").value.split('-')[1];
+      break;
+    case "1":
+      event.value = this.getField("Enchantment 1").value.split('-')[1] + " " + this.getField("ItemSubType").value;
+      break;
+  };
+}
--- nyborsjavascript.js
+++ nyborsjavascript.js
@@ -2,19 +2,20 @@ var subType = this.getField("ItemSubType");
 var enchantmentType1 = this.getField("Enchantment 1");
 var enchantmentType2 = this.getField("Enchantment 2");
 var enchantmentType3 = this.getField("Enchantment 3");
+var itemName = this.getField("Name1");
 
-// values, prefix/suffix, weapon/armor subtype, 
+// values, prefix/suffix, name, weapon/armor subtype, 
 var armorTypes = ["-Seletct Armor-", "Helmet", "Shield", "Breastplate", "Leather Armor", "Amulet"]
-var armorEnchantment1 = ["-1st Enchantment-", ["Absorption", "0-Absorption-0"], ["Adamantine", "1-Adamantine-1"], ["Agathys", "0-Agathys-0"], ["Animation", "0-Animation-2"]]
-var armorEnchantment2 = ["-2nd Enchantment-", ["Absorption", 0], "Adamantine", "Agathys", "Animation"]
-var armorEnchantment3 = ["-3rd Enchantment-", ["Absorption", 0], "Adamantine", "Agathys", "Animation"]
+var armorEnchantment1 = ["1st Enchantment", ["Absorption", "0-Absorption-0"], ["Adamantine", "1-Adamantine-1"], ["Agathys", "0-Agathys-0"], ["Animation", "0-Animation-2"]]
+var armorEnchantment2 = ["2nd Enchantment", ["Absorption", "0-Absorption-0"], ["Adamantine", "1-Adamantine-1"], ["Agathys", "0-Agathys-0"], ["Animation", "0-Animation-2"]]
+var armorEnchantment3 = ["3rd Enchantment", ["Absorption", "0-Absorption-0"], ["Adamantine", "1-Adamantine-1"], ["Agathys", "0-Agathys-0"], ["Animation", "0-Animation-2"]]
 
 
 var weaponTypes = ["-Select Weapon-", "Dagger", "Shortsword", "Longsword", "Great Axe"]
 
-var weaponEnchantment1 = ["-1st Enchantment-", "Ally’s", "Ambushing", "Answering", "Antimagic", "Arcane", "Athlete’s"]
-var weaponEnchantment2 = ["-2nd Enchantment-", "Ally’s", "Ambushing", "Answering", "Antimagic", "Arcane", "Athlete’s"]
-var weaponEnchantment3 = ["-3rd Enchantment-", "Ally’s", "Ambushing", "Answering", "Antimagic", "Arcane", "Athlete’s"]
+var weaponEnchantment1 = ["1st Enchantment", ["Ally’s", "1-Ally's-0"], ["Ambushing", "1-Ambushing-0"], ["Answering", "1-Answering-0"], ["Antimagic", "0-Antimagic-0"], ["Arcane", "1-Arcane-0"], ["Athlete’s", "1-Athlete's-0"]]
+var weaponEnchantment2 = ["2nd Enchantment", ["Ally’s", "1-Ally's"], ["Ambushing", "1-Ambushing"], ["Answering", "1-Answering"], ["Antimagic", "0-Antimagic"], ["Arcane", "1-Arcane"], ["Athlete’s", "1-Athlete's"]]
+var weaponEnchantment3 = ["3rd Enchantment", ["Ally’s", "1-Ally's"], ["Ambushing", "1-Ambushing"], ["Answering", "1-Answering"], ["Antimagic", "0-Antimagic"], ["Arcane", "1-Arcane"], ["Athlete’s", "1-Athlete's"]]
 
 if (event.willCommit) {
   switch (event.value) {
@@ -23,18 +24,21 @@ if (event.willCommit) {
       enchantmentType1.setItems(armorEnchantment1);
       enchantmentType2.setItems(armorEnchantment2);
       enchantmentType3.setItems(armorEnchantment3);
+      itemName.value = " "
       break;
     case "Weapon":
       subType.setItems(weaponTypes);
       enchantmentType1.setItems(weaponEnchantment1);
       enchantmentType2.setItems(weaponEnchantment2);
       enchantmentType3.setItems(weaponEnchantment3);
+      itemName.value = " "
       break;
     default:
       subType.setItems(["-Item Subtype-"]);
       enchantmentType1.setItems(["-1st Enchantment-"])
       enchantmentType2.setItems(["-2nd Enchantment-"])
       enchantmentType3.setItems(["-3rd Enchantment-"])
+      itemName.value = " "
       break;
   }
 }
